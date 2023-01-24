99932801acab65bbd47c264181787490f865cc8a
PaulV13 <paulvidart@gmail.com>
2023-01-24 17:51:29 -0300

    Add arrow icon and style for the checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./normalize.css";
 import "./App.css";
 import { ReactComponent as CopyIcon } from "./assets/copy_icon.svg";
+import { ReactComponent as ArrowIcon } from "./assets/arrow_right_icon.svg";
 
 function App() {
   return (
@@ -15,34 +16,37 @@ function App() {
           <p>Character Length</p>
           <p className="card-character-number">10</p>
         </div>
-        <div className="checkbox">
-          <input type="checkbox" />
-          <span>Include Uppercase letters</span>
+        <div className="checkbox-container">
+          <input type="checkbox" id="checkbox-uppercase" />
+          <label for="checkbox-uppercase">Include Uppercase letters</label>
         </div>
-        <div className="checkbox">
-          <input type="checkbox" />
-          <span>Include Lowercase letters</span>
+        <div className="checkbox-container">
+          <input type="checkbox" id="checkbox-lowercase" />
+          <label for="checkbox-lowercase">Include Lowercase letters</label>
         </div>
-        <div className="checkbox">
-          <input type="checkbox" />
-          <span>Include Numbers</span>
+        <div className="checkbox-container">
+          <input type="checkbox" id="checkbox-numbers" />
+          <label for="checkbox-numbers">Include Numbers</label>
         </div>
-        <div className="checkbox">
-          <input type="checkbox" />
-          <span>Include Symbols</span>
+        <div className="checkbox-container">
+          <input type="checkbox" id="checkbox-symbols" />
+          <label for="checkbox-symbols">Include Symbols</label>
         </div>
         <div className="card-strength">
           <div className="strength">STRENGTH</div>
           <div className="card-strength-icons">
             <div>MEDIUM</div>
-            <span className="strength-icon"></span>
+            <span className="strength-icon active"></span>
             <span className="strength-icon"></span>
             <span className="strength-icon"></span>
             <span className="strength-icon"></span>
           </div>
         </div>
 
-        <button className="btn-generate"> Generate </button>
+        <button className="btn-generate">
+          <span> GENERATE </span>
+          <ArrowIcon className="arrow_icon" />
+        </button>
       </section>
     </div>
   );
