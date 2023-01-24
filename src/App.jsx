94dce9214bb2c582e3adc5656e3267b7749eee41
PaulV13 <paulvidart@gmail.com>
2023-01-24 18:45:29 -0300

    Add range slider for character length

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./normalize.css";
 import "./App.css";
 import { ReactComponent as CopyIcon } from "./assets/copy_icon.svg";
 import { ReactComponent as ArrowIcon } from "./assets/arrow_right_icon.svg";
 
 function App() {
+  const [length, setLength] = useState(10);
+
+  const handleChangeLength = (e) => {
+    setLength(e.target.valueAsNumber);
+  };
+
+  const getBackgroundSize = () => {
+    return {
+      backgroundSize: `${(length * 100) / 20}% 100%`,
+    };
+  };
+
   return (
     <div className="container">
       <h2 className="title">Password Generate</h2>
@@ -14,23 +27,32 @@ function App() {
       <section className="card">
         <div className="card-character-length">
           <p>Character Length</p>
-          <p className="card-character-number">10</p>
+          <p className="card-character-number">{length}</p>
         </div>
+        <input
+          min={0}
+          max={20}
+          type="range"
+          className="range"
+          value={length}
+          onChange={handleChangeLength}
+          style={getBackgroundSize()}
+        />
         <div className="checkbox-container">
           <input type="checkbox" id="checkbox-uppercase" />
-          <label for="checkbox-uppercase">Include Uppercase letters</label>
+          <label htmlFor="checkbox-uppercase">Include Uppercase letters</label>
         </div>
         <div className="checkbox-container">
           <input type="checkbox" id="checkbox-lowercase" />
-          <label for="checkbox-lowercase">Include Lowercase letters</label>
+          <label htmlFor="checkbox-lowercase">Include Lowercase letters</label>
         </div>
         <div className="checkbox-container">
           <input type="checkbox" id="checkbox-numbers" />
-          <label for="checkbox-numbers">Include Numbers</label>
+          <label htmlFor="checkbox-numbers">Include Numbers</label>
         </div>
         <div className="checkbox-container">
           <input type="checkbox" id="checkbox-symbols" />
-          <label for="checkbox-symbols">Include Symbols</label>
+          <label htmlFor="checkbox-symbols">Include Symbols</label>
         </div>
         <div className="card-strength">
           <div className="strength">STRENGTH</div>
